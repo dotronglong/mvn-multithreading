55c2e8d2c2411193f4170f7036d53363d285b32e
Long Do <long.do@audiencemedia.com>
2016-07-20 10:50:38 +0700

    Update command

--- src/main/java/com/dotronglong/multithreading/Command.java
+++ src/main/java/com/dotronglong/multithreading/Command.java
@@ -1,31 +1,6 @@
 package com.dotronglong.multithreading;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-class StreamGobbler extends Thread {
-    InputStream is;
-    String type;
-
-    StreamGobbler(InputStream is, String type) {
-        this.is = is;
-        this.type = type;
-    }
-
-    public void run() {
-        try {
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String line = null;
-            while ((line = br.readLine()) != null)
-                System.out.println(type + ">" + line);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-}
+import com.dotronglong.multithreading.util.StreamProcessor;
 
 public class Command {
     public String cmd;
@@ -35,60 +10,26 @@ public class Command {
     }
 
     public void run() {
-        runGoodCommand();
+        executeCommand();
     }
 
-    private void runCommand() {
-        String s = null;
-
+    private void executeCommand() {
         try {
-            // run the Unix command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec(cmd);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(p.getErrorStream()));
-
-            // read the output from the command
-            System.out.println("→ " + cmd);
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-
-            // read any errors from the attempted command
-            System.out.println();
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-        } catch (IOException e) {
-            System.out.println("exception happened - here's what I know: ");
-            e.printStackTrace();
-            System.exit(-1);
-        }
-    }
-
-    private void runGoodCommand() {
-        try {
-            Process proc = Runtime.getRuntime().exec(this.cmd);
+            Process p = Runtime.getRuntime().exec(this.cmd);
 
             // any error message?
-            StreamGobbler errorGobbler = new
-                    StreamGobbler(proc.getErrorStream(), "ERROR");
+            StreamProcessor errorStream = new
+                    StreamProcessor(p.getErrorStream(), "ERROR");
 
             // any output?
-            StreamGobbler outputGobbler = new
-                    StreamGobbler(proc.getInputStream(), "OUTPUT");
+            StreamProcessor outputStream = new
+                    StreamProcessor(p.getInputStream(), "OUTPUT");
 
             // kick them off
-            errorGobbler.start();
-            outputGobbler.start();
+            errorStream.start();
+            outputStream.start();
 
-            // any error???
-            int exitVal = proc.waitFor();
-            System.out.println("ExitValue: " + exitVal);
+//            System.exit(p.waitFor());
         } catch (Throwable t) {
             t.printStackTrace();
         }
---
+++ src/main/java/com/dotronglong/multithreading/util/StreamProcessor.java
@@ -0,0 +1,61 @@
+package com.dotronglong.multithreading.util;
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 Do Trong Long
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * StreamProcessor
+ * <p>
+ * Handle and send out the output of buffer stream
+ *
+ * @author Long Do
+ * @version 1.0.0
+ * @since Jul 20, 2016
+ */
+public class StreamProcessor extends Thread {
+    InputStream is;
+    String type;
+
+    public StreamProcessor(InputStream is, String type) {
+        this.is = is;
+        this.type = type;
+    }
+
+    public void run() {
+        try {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
